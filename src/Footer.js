0c192eea3f610284f14b2857db3b39a8a613d934
ash1day <y4ashida@gmail.com>
2016-09-18 05:38:01 +0900

    Add keys to links

--- src/Footer.js
+++ src/Footer.js
@@ -8,7 +8,7 @@ class Footer extends Component {
       { name: 'Yoshihiro Ashida', github_id: 'ash1day' },
       { name: 'Yusuke Sangenya',  github_id: 'genya0407' }
     ].map((auther) => {
-      return <a href={`https://github.com/${auther.github_id}`} target="_blank">{ auther.name }</a>
+      return <a key={auther.name} href={`https://github.com/${auther.github_id}`} target="_blank">{ auther.name }</a>
     }).reduce((accu, elem) => {
       return accu === null ? [elem] : [...accu, ', ', elem]
     }, null);
