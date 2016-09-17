285b3bee9701651eb8314f10b9d4e3345e6daf0b
Yusuke Sangenya <longinus.eva@gmail.com>
2016-09-18 02:16:38 +0900

    refactored a bit

--- src/generate_json.js
+++ src/generate_json.js
@@ -1,66 +1,64 @@
 const graph = require('./library_graph.json');
 const fs = require('fs');
 
-const central_group = "computing";
-
-const base_size = 80;
-const group_circle_radius_rate = 1;
-const big_circle_radius_rate = 3;
-const group_circle_radius = group_circle_radius_rate * base_size;
-const big_circle_radius = big_circle_radius_rate * base_size;
-
 function rotate(x, y, rad){
 	const x1 = Math.cos(rad) * x - Math.sin(rad) * y;
 	const y1 = Math.cos(rad) * y + Math.sin(rad) * x;
     return { x: x1, y: y1 };
 }
 
+// library group rendered at center of the circle
+const central_group = "computing";
+
+// groups of libraries
 let all_groups = ["python", "ruby"].map((lang)=>{ return graph[`${lang}_libraries`]; })
                                    .reduce((first_libs, second_libs)=>{ return first_libs.concat(second_libs); })
                                    .map((lib)=>{ return lib.group ; })
                                    .filter((group_name)=>{ return (group_name !== undefined) ; });
 all_groups = Array.from(new Set(all_groups));
+
+// library groups except central_group
 const groups = all_groups.filter((group_name)=>{ return (group_name !== central_group) ; });
 
-console.log(all_groups);
-console.log(groups);
+// radius of each nodes, and radius of the whole circle
+const base_size = 80;
+const group_circle_radius_rate = 1;
+const big_circle_radius_rate = 3;
+const group_circle_radius = group_circle_radius_rate * base_size;
+const big_circle_radius = big_circle_radius_rate * base_size;
 
 ["python", "ruby"].map((lang)=>{
-    let hash = {}
+    const libraries = graph[`${lang}_libraries`];
+    const edges = graph[`${lang}_edges`];
+    let hash = { "elements": { "nodes": [], "edges": [] } };
 
-    hash["elements"] = { "nodes": [], "edges": [] };
-
-    const central_nodes = graph[`${lang}_libraries`]
+    const central_nodes = libraries
         .filter((lib)=>{ return ( lib.group === central_group ) ;})
         .map((lib, index, arr)=>{
-        let x = 0;
-        let y = group_circle_radius;
-        const rad = (index / arr.length) * Math.PI * 2;
-        const position = rotate(x, y, rad);
-        return { lib, position };
-    }).map(({ lib, position })=>{
-        return {
-            data: {
-                id: lib["name"],
-                name: lib["name"],
-                group: all_groups.indexOf(lib.group),
-            },
-            position,
-        };
-    });
+            const rad = (index / arr.length) * Math.PI * 2;
+            const position = rotate(0, group_circle_radius, rad);
+            return { lib, position };
+        }).map(({ lib, position })=>{
+            return {
+                data: {
+                    id: lib.name,
+                    name: lib.name,
+                    group: all_groups.indexOf(lib.group),
+                },
+                position,
+            };
+        });
     
-    hash["elements"]["nodes"] = hash["elements"]["nodes"].concat(central_nodes);
+    hash.elements.nodes = hash.elements.nodes.concat(central_nodes);
     
     // merginal groups
     groups.map((group, group_index, group_arr)=>{
-        let group_libs = graph[`${lang}_libraries`].filter((lib)=>{ return lib.group === group });
+        let group_libs = libraries.filter((lib)=>{ return lib.group === group });
 
         group_libs = group_libs.map((lib, index, arr)=>{
             // circle layout at center
-            let x = 0;
-            let y = group_circle_radius;
             const rad = (index / arr.length) * Math.PI * 2;
-            const position = rotate(x, y, rad);
+            const position = rotate(0, group_circle_radius, rad);
             return { lib, position };
         }).map(({lib, position: {x, y}})=>{
             // parallel shift
@@ -74,23 +72,23 @@ console.log(groups);
         });
         
 
-        hash["elements"]["nodes"] = hash["elements"]["nodes"].concat(group_libs.map(({lib, position})=>{
+        hash.elements.nodes = hash.elements.nodes.concat(group_libs.map(({lib, position})=>{
             return {
                 data: {
-                    id: lib["name"],
-                    name: lib["name"],
+                    id: lib.name,
+                    name: lib.name,
                     group: all_groups.indexOf(lib.group),
                 },
                 position,
             };
         }));
     });
-    hash["elements"]["edges"] = graph[`${lang}_edges`].map((edge)=>{
+    hash.elements.edges = edges.map((edge)=>{
         return {
             data: {
-                id: `${edge["source"]}_${edge["target"]}`,
-                source: edge["source"],
-                target: edge["target"],
+                id: `${edge.source}_${edge.target}`,
+                source: edge.source,
+                target: edge.target,
                 group: 1,
             }
         };
